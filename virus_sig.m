abc70972652e4c5ed5acd37e32491eb070375b26
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2023-06-02 16:02:31 +0200

    Update virus_sig.m

--- virus_sig.m
+++ virus_sig.m
@@ -1,19 +1,24 @@
 % Simulation for CyberAttk. Computer virus simulation signal modulation
 close all;
 clear;
+% zero-padding
+zeropad=16;
+
 %% Step 1: Generate a random stream of computer code data to simulate a virus
 % code data generation
 num_bits=200; % number of binary digits
 code_data=randi([0,1],1,num_bits);  % generate random binary data
 
-disp(code_data);
+%disp(code_data);
 
 %% Step 2: Generate a modulation signal
 % modulation parameters
-f0=120e9; % Center freq: 120 GHz
+%%% adjuste according to the actual situation %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+f0=120e9; % Center freq: 120 GHz - THz radar transmitter
 fs=10000; % sampling/symbol rate (number of symbols transmitted per s)
 modIndex=100; % modulation index that controls the range of frequency variation
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 t=0:1/fs:length(code_data)-1/fs;
 mod_sig=zeros(size(t));
 for i=1:length(code_data)
@@ -21,9 +26,6 @@ for i=1:length(code_data)
         mod_sig((i-1)*fs+1:i*fs)=1;
     end
 end
-
-% zero-padding
-zeropad=16;
 
 TMs=fft(mod_sig,length(mod_sig)*zeropad);
 figure;
