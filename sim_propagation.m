4e5b81d5c9978ef1fb112fd7a7c332707f97f266
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2023-06-04 00:02:46 +0200

    Add files via upload

---
+++ sim_propagation.m
@@ -0,0 +1,20 @@
+% Simulation of Computer Virus Propagation Mathematical Model
+clear all;
+close all;
+
+% Parameters setting
+N=100; % number of computers
+M=15; % number of data communications between computers
+X0=1; % initial number of infected computers
+
+% Time generation
+num_time_units=100; % number of time unit for the simulation
+time=1:num_time_units; % timeline
+
+% Mathematical expectation of propagation
+Xn=N./(1+(N-1)*exp(-time*M/N));
+
+% make plot...
+plot(time,Xn)
+xlabel('Time')
+ylabel('Number of virus-infected computers')
