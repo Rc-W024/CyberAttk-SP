b1f5d834e20bdb19b0c35e41abc222dfd68d0c54
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2023-06-08 12:56:53 +0200

    Add files via upload

---
+++ virus_sig.m
@@ -0,0 +1,81 @@
+% Simulation for CyberAttk. Computer virus simulation signal modulation
+close all;
+clear;
+
+%%% adjuste according to the actual situation %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fs=1000; % sampling frequecy
+fc=120e9; % center freq: 120 GHz - THz radar transmitter
+bitRate=1e6; % data rate: 1 Mbps
+numBits=200; % number of binary digits
+zeropad=16; % zero-padding
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Step 1: Generate a random stream of computer code data to simulate a virus
+% code data generation
+data_binary=randi([0 1],1,numBits);  % generate random binary data
+bitDuration=1/bitRate; % duration of each bit of data
+
+%disp(code_data);
+
+%% Step 2: Generate a carrier signal
+% time vector
+t=linspace(0,bitDuration*length(data_binary),length(data_binary)*fs);
+
+% carrier signal
+carrierSig=cos(2*pi*fc*t);
+
+% FFT
+FFTc=fft(carrierSig,length(carrierSig)*zeropad);
+figure;
+plot(abs(FFTc));
+title('FFT of carrier signal')
+
+%% Step 3: FSK modulation
+modulatedSig=[];
+for i = 1:length(data_binary)
+    if data_binary(i) == 0
+        modulatedSig=[modulatedSig cos(2*pi*(fc-bitRate/2)*t((i-1)*fs+1:i*fs))];
+    else
+        modulatedSig=[modulatedSig cos(2*pi*(fc+bitRate/2)*t((i-1)*fs+1:i*fs))];
+    end
+end
+
+% FFT
+FFTm=fft(modulatedSig,length(modulatedSig)*zeropad);
+figure;
+plot(abs(FFTm));
+title('FFT of modulated signal')
+
+% emitted signal generation
+emittedSig=modulatedSig+carrierSig;
+
+% FFT
+FFTe=fft(emittedSig,length(emittedSig)*zeropad);
+figure;
+plot(abs(FFTe));
+title('FFT of emitted signal')
+
+%% Print figures...
+figure;
+plot(t,carrierSig);
+xlabel('Time');
+ylabel('Amplitude');
+title('Carrier Signal');
+
+figure;
+plot(t,modulatedSig);
+xlabel('Time');
+ylabel('Amplitude');
+title('Modulated Signal');
+
+figure;
+plot(t,emittedSig);
+xlabel('Time');
+ylabel('Amplitude');
+title('Emitted Signal');
+
+figure;
+spectrogram(emittedSig,hann(256),250,1024,fs,'yaxis');
+title('Spectrum');
+
